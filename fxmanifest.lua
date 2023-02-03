6097f1cdac7bc7a0d217e482ef1ea2bb44f01fe1
DyRose <64310374+DyroS3@users.noreply.github.com>
2023-02-03 10:39:41 +0800

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,8 +5,8 @@ game 'gta5'
 lua54 'yes'
 
 shared_scripts {
-    '@ox_lib/init.lua',
-	'@es_extended/imports.lua'
+    '@ox_lib/init.lua',             
+    '@es_extended/imports.lua'          -- 适配ESX
 }
 
 client_script '3Dtext.lua'
